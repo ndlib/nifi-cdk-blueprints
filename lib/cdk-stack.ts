d5087040eaf9bdfc66eb8a66c93a1d92a763e7b4
Justin Rittenhouse <jrittenh@nd.edu>
2020-10-06 18:07:38 -0400

    Commit basic infrastructure setup

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,9 +1,87 @@
-import * as cdk from '@aws-cdk/core';
+import codebuild = require('@aws-cdk/aws-codebuild');
+import { Artifact, Pipeline } from '@aws-cdk/aws-codepipeline';
+import { GitHubSourceAction, CodeBuildAction, ManualApprovalAction } from '@aws-cdk/aws-codepipeline-actions';
+import { Construct, Duration, FileSystem, Fn, RemovalPolicy, SecretValue, Stack, StackProps } from '@aws-cdk/core';
+import { Vpc, SecurityGroup } from '@aws-cdk/aws-ec2';
+import { PolicyStatement } from '@aws-cdk/aws-iam';
+import { LogGroup, RetentionDays } from '@aws-cdk/aws-logs';
+import { Bucket, BucketAccessControl } from '@aws-cdk/aws-s3';
+import { PrivateDnsNamespace } from '@aws-cdk/aws-servicediscovery';
+import { Topic } from '@aws-cdk/aws-sns';
+import { ArtifactBucket, HttpsAlb, SlackApproval } from '@ndlib/ndlib-cdk';
 
-export class CdkStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export interface NiFiAppInfrastructureProps extends StackProps {
+  readonly owner: string;
+  readonly contact: string;
+  readonly networkStackName: string;
+  readonly serviceName: string;
+  readonly domainStack: string;
+}
+
+export class NiFiAppInfrastructureStack extends Stack {
+  public readonly loadBalancer: HttpsAlb;
+  public readonly logs: LogGroup;
+  public readonly vpc: Vpc;
+  public readonly logBucket: Bucket;
+  public readonly privateNamespace: PrivateDnsNamespace;
+  constructor(scope: Construct, id: string, props: NiFiAppInfrastructureProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const owner = props.owner || `see stack: ${this.stackName}`;
+    const contact = props.contact || `see stack: ${this.stackName}`;
+
+    const vpcId = Fn.importValue(`${props.networkStackName}:VPCID`)
+    const vpc = Vpc.fromVpcAttributes(this, 'peered-network', {
+      vpcId: vpcId,
+      availabilityZones: [
+        Fn.select(0, Fn.getAzs()),
+        Fn.select(1, Fn.getAzs()),
+      ],
+      publicSubnetIds: [
+        Fn.importValue(`${props.networkStackName}:PublicSubnet1ID`),
+        Fn.importValue(`${props.networkStackName}:PublicSubnet2ID`),
+      ],
+      privateSubnetIds: [
+        Fn.importValue(`${props.networkStackName}:PrivateSubnet1ID`),
+        Fn.importValue(`${props.networkStackName}:PrivateSubnet2ID`),
+      ],
+    });
+
+    this.loadBalancer = new HttpsAlb(this, 'NiFiLoadBalancer', {
+      certificateArns: [ Fn.importValue(`${props.domainStack}:ACMCertificateARN`) ],
+      vpc: vpc,
+      internetFacing: true,
+    });
+
+    this.logBucket = new Bucket(this, 'NiFiLogBucket', {
+      accessControl: BucketAccessControl.LOG_DELIVERY_WRITE,
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      lifecycleRules: [{
+        enabled: true,
+        expiration: Duration.days(365 * 10),
+        noncurrentVersionExpiration: Duration.days(1),
+      }],
+    });
+
+    this.logs = new LogGroup(this, 'NiFiSharedLogGroup', {
+      retention: RetentionDays.ONE_MONTH,
+      logGroupName: this.stackName,
+      removalPolicy: RemovalPolicy.DESTROY,
+    });
+
+    this.privateNamespace = new PrivateDnsNamespace(this, `${props.serviceName}-namespace`, {
+      vpc: vpc,
+      name: `${props.serviceName}`,
+      description: `Private Namespace for ${props.serviceName}`,
+    });
+
+    const NiFiLoadBalancerSecurityGroup = new SecurityGroup(this, 'NiFiLoadBalancerSecurityGroup', {
+      vpc: vpc,
+      allowAllOutbound: true,
+      description: 'Access to the public facing load balancer',
+      securityGroupName: this.stackName
+    });
+
   }
 }
