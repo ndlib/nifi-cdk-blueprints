a7aad926feb54079da5851c4552818f1c3f1c0c4
Justin Rittenhouse <jrittenh@nd.edu>
2020-10-09 14:01:16 -0400

    Clean up imports for infrastructure

--- lib/app-infrastructure-stack.ts
+++ lib/app-infrastructure-stack.ts
@@ -1,15 +1,10 @@
-import codebuild = require('@aws-cdk/aws-codebuild');
-import { Artifact, Pipeline } from '@aws-cdk/aws-codepipeline';
-import { GitHubSourceAction, CodeBuildAction, ManualApprovalAction, ServiceCatalogDeployAction } from '@aws-cdk/aws-codepipeline-actions';
-import { Construct, Duration, FileSystem, Fn, RemovalPolicy, SecretValue, Stack, StackProps } from '@aws-cdk/core';
+import { Construct, Duration, Fn, RemovalPolicy, Stack, StackProps } from '@aws-cdk/core';
 import { Vpc, SecurityGroup } from '@aws-cdk/aws-ec2';
 import { Cluster } from '@aws-cdk/aws-ecs';
-import { PolicyStatement } from '@aws-cdk/aws-iam';
 import { LogGroup, RetentionDays } from '@aws-cdk/aws-logs';
 import { Bucket, BucketAccessControl } from '@aws-cdk/aws-s3';
 import { NamespaceType, PrivateDnsNamespace, Service } from '@aws-cdk/aws-servicediscovery';
-import { Topic } from '@aws-cdk/aws-sns';
-import { ArtifactBucket, HttpsAlb, SlackApproval } from '@ndlib/ndlib-cdk';
+import { HttpsAlb } from '@ndlib/ndlib-cdk';
 
 export interface NiFiAppInfrastructureProps extends StackProps {
   readonly owner: string;
