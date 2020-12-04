d196be8be6bad0f7237ff1a16926b423a6bb2643
Justin Rittenhouse <jrittenh@nd.edu>
2020-12-04 16:55:11 -0500

    Track context helpers and types scripts

---
+++ src/context-helpers.ts
@@ -0,0 +1,22 @@
+import { ConstructNode } from '@aws-cdk/core';
+
+export const getContextByNamespace = (namespace: string): any => {
+    const allContext = JSON.parse(process.env.CDK_CONTEXT_JSON ?? '{}');
+    const result: any = {};
+    const prefix = `${namespace}`;
+    for (const [key, value] of Object.entries(allContext)) {
+        if (key.startsWith(prefix)) {
+            const flattenedKey = key.substr(prefix.length);
+            result[flattenedKey] = value;
+        }
+    }
+    return result;
+}
+
+export const getRequiredContext = (node: ConstructNode, key: string): any => {
+    const value = node.tryGetContext(key);
+    if (value === undefined || value === null) {
+        throw new Error(`Context key '${key}' is required.`);
+    }
+    return value;
+}
