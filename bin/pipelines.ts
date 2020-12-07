1cc86f554d51c263fd41993fb57a7c656e403e1c
Justin Rittenhouse <jrittenh@nd.edu>
2020-12-07 18:12:14 -0500

    Rewrite bin

--- bin/pipelines.ts
+++ bin/pipelines.ts
@@ -1,1 +1,7 @@
-import { NiFiCAPipeline } from '../src/nifi-ca-pipeline-stack';
+import { App } from '@aws-cdk/core';
+import { CustomEnvironment } from '../src/custom-environment';
+import { Stacks } from '../src/types';
+
+export const instantiateStacks = (app: App, namespace: string, env: CustomEnvironment, testStacks: Stacks, prodStacks: Stacks): Stacks => {
+    return {}
+}
